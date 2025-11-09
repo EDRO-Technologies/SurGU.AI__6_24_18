a53b142b7f7b6a34858290d8f45f73ed8a7481ea
Akkato <unknown.255.galactic.acc@gmail.com>
2025-11-09 09:13:58 +0500

    done?

--- dist/lib/reponse.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendResponse = void 0;
-const sendResponse = (res, statusCode, message) => {
-    res.status(statusCode).json({ statusCode, message });
-};
-exports.sendResponse = sendResponse;
